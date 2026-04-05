14ae2444adf706eb32839edc68e545048d51c54e
Afelipe72 <87008382+Afelipe72@users.noreply.github.com>
2026-04-04 23:44:19 -0500

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public List<List<string>> GroupAnagrams(string[] strs) {
+        Dictionary<string, List<string>> anagrams = new Dictionary<string, List<string>>();
+        foreach(string word in strs)
+        {
+            char[] charArray = word.ToCharArray();
+            Array.Sort(charArray);
+            string key = new string(charArray);
+
+            if(!anagrams.ContainsKey(key))
+            {
+                anagrams[key] = new List<string>();
+            }
+
+            anagrams[key].Add(word);
+
+
+        }
+        
+        return anagrams.Values.ToList();
+    }
+}
