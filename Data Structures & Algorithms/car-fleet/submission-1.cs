1c07662ae350e4ffa6baee681957494e4b8c26a5
Afelipe72 <87008382+Afelipe72@users.noreply.github.com>
2026-06-03 01:11:53 -0500

    Add: car-fleet - submission-1

---
+++ Data Structures & Algorithms/car-fleet/submission-1.cs
@@ -0,0 +1,32 @@
+public class Solution {
+    // this is so dumb im going to forget this in like 5 mins
+    public int CarFleet(int target, int[] position, int[] speed) {
+
+        int n = position.Length;
+
+        int[][] cars = new int[n][];
+
+        for (int i = 0; i < n; i++)
+        {
+            cars[i] = new int[] { position[i], speed[i] };
+        }
+
+        Array.Sort(cars, (a, b) => b[0].CompareTo(a[0]));
+
+        int fleets = 0;
+        double lastFleetTime = 0;
+
+        foreach (var car in cars)
+        {
+            double time = (double)(target - car[0]) / car[1];
+
+            if (time > lastFleetTime)
+            {
+                fleets++;
+                lastFleetTime = time;
+            }
+        }  
+
+        return fleets;
+    }
+}
